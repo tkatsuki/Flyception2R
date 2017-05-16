ed471fe2d8de6194444e8b948bf3e4bb7d77bab9
Takeo Katsuki <takeo.katsuki@gmail.com>
2017-05-16 06:23:26 -0700

    Added Windows compatibility

--- R/imageJ_crop_append.R
+++ R/imageJ_crop_append.R
@@ -8,13 +8,23 @@
 #' imageJ_crop_append()
 
 imageJ_crop_append <- function(dir, ch=1, roi=c(383, 0, 256, 256)){
+  
+  os <-.Platform$OS.type
+  
   # Crop a ROI for each file
-  fluo_view_files <- list.files(dir, pattern="ome\\.tif$") # The first file is created without a numeric extension
+  fluo_view_files <- list.files(dir, pattern="ome\\.tif$", full.names=T) # The first file is created without a numeric extension
   file_order <- c(length(fluo_view_files), 1:(length(fluo_view_files)-1)) # Therefore we need to bring it front
   for(s in file_order){
     macro <- paste0('open("',fluo_view_files[s],'");\n makeRectangle(', paste(roi, collapse=","), ');\n run("Crop");\n saveAs("tiff", "',tools::file_path_sans_ext(fluo_view_files[s]),'.ch',ch,'.crop.tif");\n run("Quit");\n')
-    write(macro, file="macro.txt")
-    system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -batch macro.txt"), wait=T) 
+    write(macro, file=paste0(dir,"macro.txt"))
+    if (os == "windows"){
+      bat <- paste0('pushd "C:\\Program Files\\ImageJ"', '\n jre\\bin\\java -jar -Xmx1024m ij.jar  -batch "H:\\P1_GCaMP6s_tdTomato_041117\\P1-Gal4_UAS-GCaMP6s_tdTomato_test_8\\macro.txt" ', dir, '\n pause\n exit')
+      tempbat <- paste(tempfile('bat'),".bat",sep="")
+      write(bat, file=tempbat)
+      shell(tempbat,wait=T)   
+    }else{
+      system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -batch ", dir, "macro.txt"), wait=T) 
+    }
   }
   
   # Load cropped videos
