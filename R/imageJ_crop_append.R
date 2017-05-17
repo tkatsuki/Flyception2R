8e87d558cbd38aab7b995667638929c30e77dc11
Takeo Katsuki <takeo.katsuki@gmail.com>
2017-05-17 09:46:32 -0700

    Working code for windows

--- R/imageJ_crop_append.R
+++ R/imageJ_crop_append.R
@@ -18,7 +18,7 @@ imageJ_crop_append <- function(dir, ch=1, roi=c(383, 0, 256, 256)){
     macro <- paste0('open("',fluo_view_files[s],'");\n makeRectangle(', paste(roi, collapse=","), ');\n run("Crop");\n saveAs("tiff", "',tools::file_path_sans_ext(fluo_view_files[s]),'.ch',ch,'.crop.tif");\n run("Quit");\n')
     write(macro, file=paste0(dir,"macro.txt"))
     if (os == "windows"){
-      bat <- paste0('pushd "C:\\Program Files\\ImageJ"', '\n jre\\bin\\java -jar -Xmx1024m ij.jar  -batch "H:\\P1_GCaMP6s_tdTomato_041117\\P1-Gal4_UAS-GCaMP6s_tdTomato_test_8\\macro.txt" ', dir, '\n pause\n exit')
+      bat <- paste0('pushd "C:\\Program Files\\ImageJ"', '\n jre\\bin\\java -jar -Xmx8g ij.jar  -batch "H:\\P1_GCaMP6s_tdTomato_041117\\P1-Gal4_UAS-GCaMP6s_tdTomato_test_8\\macro.txt" ', dir, '\n pause\n exit')
       tempbat <- paste(tempfile('bat'),".bat",sep="")
       write(bat, file=tempbat)
       shell(tempbat,wait=T)   
@@ -28,14 +28,15 @@ imageJ_crop_append <- function(dir, ch=1, roi=c(383, 0, 256, 256)){
   }
   
   # Load cropped videos
+  fluo_view_cropped_files_full <- list.files(dir, pattern=paste0("ome\\.ch", ch, "\\.crop\\.tif$"),  full.names=T)
   fluo_view_cropped_files <- list.files(dir, pattern=paste0("ome\\.ch", ch, "\\.crop\\.tif$"))
   cropped_file_order <- c(length(fluo_view_cropped_files), 1:(length(fluo_view_cropped_files)-1))
   
-  for(cr in 1:length(fluo_view_cropped_files)){
+  for(cr in 1:length(fluo_view_cropped_files_full)){
     if(cr == 1){
-      write(paste0('open("',fluo_view_cropped_files[cropped_file_order[cr]],'");\n'), file="macro.txt")
+      write(paste0('open("',fluo_view_cropped_files_full[cropped_file_order[cr]],'");\n'), file=paste0(dir,"macro.txt"))
     }else{
-      write(paste0('open("',fluo_view_cropped_files[cropped_file_order[cr]],'");\n'), file="macro.txt", append=T)
+      write(paste0('open("',fluo_view_cropped_files_full[cropped_file_order[cr]],'");\n'), file=paste0(dir,"macro.txt"), append=T)
     }
   }
   
@@ -44,11 +45,19 @@ imageJ_crop_append <- function(dir, ch=1, roi=c(383, 0, 256, 256)){
   for(st in 1:length(fluo_view_cropped_files)){
     strs[st] <- paste0('image',st,'=',fluo_view_cropped_files[cropped_file_order[st]])
   }
-  write(paste('run("Concatenate...", "  title=[Concatenated Stacks]', paste(strs, collapse=" "), '");\n'), file="macro.txt", append=T)
-  write(paste0('saveAs("tiff", "', tools::file_path_sans_ext(fluo_view_cropped_files[length(fluo_view_cropped_files)]), '.concat.tif");\n run("Quit");\n'), file="macro.txt", append=T)
+  write(paste('run("Concatenate...", "  title=[Concatenated Stacks]', paste(strs, collapse=" "), '");\n'), file=paste0(dir,"macro.txt"), append=T)
+  write(paste0('saveAs("tiff", "', tools::file_path_sans_ext(fluo_view_cropped_files[length(fluo_view_cropped_files)]), '.concat.tif");\n run("Quit");\n'), file=paste0(dir,"macro.txt"), append=T)
   
   # Execute the macro
-  system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -batch macro.txt"), wait=T) 
   
+  if (os == "windows"){
+    bat <- paste0('pushd "C:\\Program Files\\ImageJ"', '\n jre\\bin\\java -jar -Xmx8g ij.jar  -batch "H:\\P1_GCaMP6s_tdTomato_041117\\P1-Gal4_UAS-GCaMP6s_tdTomato_test_8\\macro.txt" ', dir, '\n pause\n exit')
+    tempbat <- paste(tempfile('bat'),".bat",sep="")
+    write(bat, file=tempbat)
+    shell(tempbat,wait=T)   
+  }else{
+
+  system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ -batch macro.txt"), wait=T) 
+  }
 }
 
