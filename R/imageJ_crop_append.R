67b1d2e88a80d3bdab38f7f3863ee7b87b38ad39
Thomas J Dawkins <tjdawkins@redtreeit.com>
2019-05-29 12:26:05 -0700

    Add support for specify outputs directory / clean up byte-order code

--- R/imageJ_crop_append.R
+++ R/imageJ_crop_append.R
@@ -1,32 +1,23 @@
 #' Crop and concatenate fluo_view image with up to 2 channels using ImageJ
 #'
 #' @param dir path to the directory that contains the data
+#' @param outdir path to directory in where to save outputs (defaults to dir)
 #' @param ch channel name used for output files
 #' @param roi roi coordinates (x, y, w, h), note origin is (0,0)
 #' @export
 #' @examples
 #' imageJ_crop_append()
 
-imageJ_crop_append <- function(dir, ch=1, roi=c(383, 0, 256, 256)){
+imageJ_crop_append <- function(dir, outdir=NA, ch=1, roi=c(383, 0, 256, 256)){
   
   os <-.Platform$OS.type
-  windir <- gsub("/", "\\", dir, fixed=TRUE)
   
-  # Configure ImageJ so that TIFF is saved in little endian
-  macro <- paste0('run("Input/Output...", "jpeg=85 gif=-1 file=.xls use_file save copy_row save_column save_row");\n')
-  write(macro, file=paste0(dir,"macro1.txt"))
-  if (os == "windows"){
-    bat <- paste0('pushd "C:\\Program Files\\ImageJ"', '\n jre\\bin\\java -jar -Xmx8g ij.jar  -batch "', windir, 'macro1.txt" ', dir, '\n pause\n exit')
-    tempbat <- paste(tempfile('bat'),".bat",sep="")
-    write(bat, file=tempbat)
-    shell(tempbat, translate=T, wait=T)   
-  } else if (os == "unix") {
-    system(paste0("~/Fiji.app/ImageJ-linux64 --headless -batch ", dir, "macro1.txt"), wait=T)
-  }else{
-    system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ --headless -batch ", dir, "macro1.txt"), wait=T) 
-  }
+  if(is.na(outdir))
+    outdir <- dir
   
-  # Crop a ROI for each file
+  windir <- gsub("/", "\\", outdir, fixed=TRUE)
+
+    # Crop a ROI for each file
   fluo_view_files <- list.files(dir, pattern="ome\\.tif$", full.names=T) # The first file is created without a numeric extension
   if(length(fluo_view_files)==1){
     file_order <- 1
@@ -34,23 +25,34 @@ imageJ_crop_append <- function(dir, ch=1, roi=c(383, 0, 256, 256)){
     file_order <- c(length(fluo_view_files), 1:(length(fluo_view_files)-1)) # Therefore we need to bring it front
   }
   for(s in file_order){
-    macro <- paste0('run("Input/Output...", "jpeg=85 gif=-1 file=.csv use_file save copy_row save_column save_row");\nopen("',fluo_view_files[s],'");\n makeRectangle(', paste(roi, collapse=","), ');\n run("Crop");\n saveAs("tiff", "',tools::file_path_sans_ext(fluo_view_files[s]),'.ch',ch,'.crop.tif");\n run("Quit");\n')
-    write(macro, file=paste0(dir,"macro2.txt"))
+    macro <- paste0('run("Input/Output...", "jpeg=85 gif=-1 file=.csv use_file save copy_row save_column save_row");',
+                    '\nopen("', fluo_view_files[s],'");\n',
+                    'makeRectangle(', paste(roi, collapse=","),');\n',
+                    'run("Crop");\n saveAs("tiff", "',
+                    paste0(outdir,basename(tools::file_path_sans_ext(fluo_view_files[s]))),
+                    '.ch',ch,'.crop.tif");\n',
+                    'run("Quit");\n')
+    
+    write(macro, file=paste0(outdir,"macro1.txt"))
+    
     if (os == "windows"){
-      bat <- paste0('pushd "C:\\Program Files\\ImageJ"', '\n jre\\bin\\java -jar -Xmx8g ij.jar  -batch "', windir, 'macro2.txt" ', dir, '\n pause\n exit')
+      bat <- paste0('pushd "C:\\Program Files\\ImageJ"',
+                    '\n jre\\bin\\java -jar -Xmx8g ij.jar  -batch "',
+                    windir, 'macro1.txt" ', dir, '\n pause\n exit')
       tempbat <- paste(tempfile('bat'),".bat",sep="")
       write(bat, file=tempbat)
       shell(tempbat,wait=T)   
     } else if (os == "unix") {
-      system(paste0("~/Fiji.app/ImageJ-linux64 --headless -batch ", dir, "macro2.txt"), wait=T) 
+      system(paste0("~/Fiji.app/ImageJ-linux64 --headless -batch ", outdir, "macro1.txt"), wait=T) 
     }else{
-      system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ --headless -batch ", dir, "macro2.txt"), wait=T) 
+      system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar",
+                    " -ijpath /Applications/ImageJ ","--headless -batch ", outdir, "macro1.txt"), wait=T) 
     }
   } 
   
   # Load cropped videos
-  fluo_view_cropped_files_full <- list.files(dir, pattern=paste0("ome\\.ch", ch, "\\.crop\\.tif$"),  full.names=T)
-  fluo_view_cropped_files <- list.files(dir, pattern=paste0("ome\\.ch", ch, "\\.crop\\.tif$"))
+  fluo_view_cropped_files_full <- list.files(outdir, pattern=paste0("ome\\.ch", ch, "\\.crop\\.tif$"),  full.names=T)
+  fluo_view_cropped_files <- list.files(outdir, pattern=paste0("ome\\.ch", ch, "\\.crop\\.tif$"))
   if(length(fluo_view_cropped_files)==1){
     return()
   }else{
@@ -59,9 +61,9 @@ imageJ_crop_append <- function(dir, ch=1, roi=c(383, 0, 256, 256)){
   
   for(cr in 1:length(fluo_view_cropped_files_full)){
     if(cr == 1){
-      write(paste0('open("',fluo_view_cropped_files_full[cr],'");\n'), file=paste0(dir,"macro3.txt"))
+      write(paste0('open("',fluo_view_cropped_files_full[cr],'");'), file=paste0(outdir,"macro2.txt"))
     }else{
-      write(paste0('open("',fluo_view_cropped_files_full[cr],'");\n'), file=paste0(dir,"macro3.txt"), append=T)
+      write(paste0('open("',fluo_view_cropped_files_full[cr],'");'), file=paste0(outdir,"macro2.txt"), append=T)
     }
   }
   # Concatenate cropped videos into one
@@ -76,20 +78,30 @@ imageJ_crop_append <- function(dir, ch=1, roi=c(383, 0, 256, 256)){
       strs[st] <- paste0('image',st,'=',fluo_view_cropped_files[cropped_file_order[st]])
     }
   }
-  write(paste('run("Concatenate...", "  title=[Concatenated Stacks]', paste(strs, collapse=" "), '");\n'), file=paste0(dir,"macro3.txt"), append=T)
-  write('run("Input/Output...", "jpeg=85 gif=-1 file=.csv use_file save copy_row save_column save_row");\n', file=paste0(dir,"macro3.txt"), append=T)
-  write(paste0('saveAs("tiff", "', tools::file_path_sans_ext(fluo_view_cropped_files_full[1]), '.concat.tif");\n run("Quit");\n'), file=paste0(dir,"macro3.txt"), append=T)
+  write(paste('run("Concatenate...", "  title=[Concatenated Stacks]',
+              paste(strs, collapse=" "), '");'),
+        file=paste0(outdir,"macro2.txt"), append=T)
+  write('run("Input/Output...", "jpeg=85 gif=-1 file=.csv use_file save copy_row save_column save_row");',
+        file=paste0(outdir,"macro2.txt"), append=T)
+  write(paste0('saveAs("tiff", "',
+               paste0(outdir,basename(tools::file_path_sans_ext(fluo_view_cropped_files_full[1]))),
+               '.concat.tif");\nrun("Quit");'),
+        file=paste0(outdir,"macro2.txt"), append=T)
   
   # Execute the macro
   if (os == "windows"){
-    bat <- paste0('pushd "C:\\Program Files\\ImageJ"', '\n jre\\bin\\java -jar -Xmx8g ij.jar  -batch "', windir, 'macro3.txt" ', dir, '\n pause\n exit')
+    bat <- paste0('pushd "C:\\Program Files\\ImageJ"',
+                  '\n jre\\bin\\java -jar -Xmx8g ij.jar  -batch "',
+                  windir, 'macro2.txt" ', outdir, '\n pause\n exit')
     tempbat <- paste(tempfile('bat'),".bat",sep="")
     write(bat, file=tempbat)
-    shell(tempbat,wait=T)   
+    shell(tempbat,wait=T)
+    
   } else if (os == "unix") {
-    system(paste0("~/Fiji.app/ImageJ-linux64 --headless -batch ", dir, "macro3.txt"), wait=T) 
+    system(paste0("~/Fiji.app/ImageJ-linux64 --headless -batch ", outdir, "macro2.txt"), wait=T) 
   } else {
     print(os)
-    system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar -ijpath /Applications/ImageJ --headless -batch ", dir, "macro3.txt"), wait=T) 
+    system(paste0("java -Xmx8g -jar /Applications/ImageJ/ImageJ.app/Contents/Resources/Java/ij.jar ",
+                  "-ijpath /Applications/ImageJ --headless -batch ", outdir, "macro2.txt"), wait=T) 
   }
 }
