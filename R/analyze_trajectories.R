7ee955582f17beeb515636ba098787861f2c2b96
tkatsuki <takeo.katsuki@gmail.com>
2019-06-11 06:56:00 +0900

    arena trajectory in mm finished

--- R/analyze_trajectories.R
+++ R/analyze_trajectories.R
@@ -18,12 +18,52 @@ analyze_trajectories <- function(dir, output, fpsfv, interaction=F){
   trja <- trja[,2:trjancol]
   trja <- as.data.frame(sapply(trja,gsub,pattern="\\[",replacement=""), stringsAsFactors=F)
   trja <- as.data.frame(sapply(trja,gsub,pattern="\\]",replacement=""), stringsAsFactors=F)
-  trja <- data.frame(sapply(trja, as.numeric))
-  data(map, package = "Flyception2R")
-  trjaint <- round(trja)
-  matchedrow <- row.match(trjaint[,1:2], map[,3:4])
-  matchedrow[which(is.na(matchedrow))] <- sapply(which(is.na(matchedrow)), function(x) which.min(abs((map[,3]-trjaint[x, 1])^2) + (map[,4]-trjaint[x, 2])^2))
-  trjav <- map[matchedrow, 1:2]
+  trja <- data.frame(sapply(trja, as.numeric)) # trajectory is in pixel coordinate
+  data(map, package = "Flyception2R") # load the pixel to degree map
+  #trjaint <- round(trja)
+  #matchedrow <- row.match(trjaint[,1:2], map[,3:4])
+  #matchedrow[which(is.na(matchedrow))] <- sapply(which(is.na(matchedrow)), function(x) which.min(abs((map[,3]-trjaint[x, 1])^2) + (map[,4]-trjaint[x, 2])^2))
+  
+  if(interaction == F){
+    matchedrow <- sapply(1:nrow(trja), function(x) which.min(abs((map[,3]-trja[x, 1])^2) + (map[,4]-trja[x, 2])^2))
+    trjadeg <- map[matchedrow, 1:2]
+    trjaalpha <- pi*trjadeg[,1]/180
+    a <- 15.174 # distance between x and y mirrors
+    b <- 68.167 # distance between y center and arena surface at the center
+    c <- 65.167 # distance between y center and top flat surface
+    trjahr <- (a+b)*cos(2*trjaalpha) - a
+    trjaxr <- a*tan(2*trjaalpha) + trjahr*tan(2*trjaalpha) # height should be h not b
+    trjabeta <- pi*trjadeg[,2]/180
+    trjyfr <- function(alpha, beta) ((a+b)*cos(2*alpha) - a)*sin(2*beta)
+    trjayr <- trjyfr(trjaalpha, trjabeta)
+    trjamm <- cbind(trjaxr, trjayr) # trajectory in mm
+  } else {
+    matchedrow <- sapply(1:nrow(trja), function(x) which.min(abs((map[,3]-trja[x, 1])^2) + (map[,4]-trja[x, 2])^2))
+    trjadeg <- map[matchedrow, 1:2]
+    trjaalpha <- pi*trjadeg[,1]/180
+    a <- 15.174 # distance between x and y mirrors
+    b <- 68.167 # distance between y center and arena surface at the center
+    c <- 65.167 # distance between y center and top flat surface
+    trjahr <- (a+b)*cos(2*trjaalpha) - a
+    trjaxr <- a*tan(2*trjaalpha) + trjahr*tan(2*trjaalpha) # height should be h not b
+    trjabeta <- pi*trjadeg[,2]/180
+    trjyfr <- function(alpha, beta) ((a+b)*cos(2*alpha) - a)*sin(2*beta)
+    trjayr <- trjyfr(trjaalpha, trjabeta)
+    trjamm <- cbind(trjaxr, trjayr) # trajectory in mm
+    
+    matchedrow <- sapply(1:nrow(trja), function(x) which.min(abs((map[,3]-trja[x, 3])^2) + (map[,4]-trja[x, 4])^2))
+    trjadeg <- map[matchedrow, 1:2]
+    trjaalpha <- pi*trjadeg[,1]/180
+    a <- 15.174 # distance between x and y mirrors
+    b <- 68.167 # distance between y center and arena surface at the center
+    c <- 65.167 # distance between y center and top flat surface
+    trjahr <- (a+b)*cos(2*trjaalpha) - a
+    trjaxr <- a*tan(2*trjaalpha) + trjahr*tan(2*trjaalpha) # height should be h not b
+    trjabeta <- pi*trjadeg[,2]/180
+    trjyfr <- function(alpha, beta) ((a+b)*cos(2*alpha) - a)*sin(2*beta)
+    trjayr <- trjyfr(trjaalpha, trjabeta)
+    trjamm <- cbind(trjamm, trjaxr, trjayr) # trajectory in mm
+  }
   
   headpos <- fvtrj[,c(4,5)]
   distance <- dipr::trackDistance(trj)
@@ -33,15 +73,15 @@ analyze_trajectories <- function(dir, output, fpsfv, interaction=F){
   png(file=paste0(output, "_error.png"), width=400, height=400)
   plot(error)
   dev.off()
-
+  
   if(interaction==T){
-    flydist <- sqrt((trja[,1] - trja[,3])^2 + (trja[,2] - trja[,4])^2)
+    flydist <- sqrt((trjamm[,1] - trjamm[,3])^2 + (trjamm[,2] - trjamm[,4])^2)
     png(file=paste0(output, "_flydist.png"), width=400, height=400)
     plot(flydist, ylab="Distance between flies (mm)")
     dev.off()
   } else {
     flydist <- NA
   }
-  return(list("speed"=speed, "error"=error, "trja"=trja, "flydist"=flydist))
+  return(list("speed"=speed, "error"=error, "trja"=trjamm, "flydist"=flydist))
   
 }
