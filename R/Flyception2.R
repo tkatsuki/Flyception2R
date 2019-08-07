d0c87934700c088ca7141b6eeb6a2f4a4df8be28
Takeo Katsuki <takeo.katsuki@gmail.com>
2019-08-07 03:23:46 -0700

    Intermediate commit

--- R/Flyception2.R
+++ R/Flyception2.R
@@ -1019,6 +1019,29 @@ Flyception2R <- function(dir, outdir=NA, autopos=T, interaction=T, stimulus=F, r
   mtext(side = 4, line = 3, 'Fluorescence intensity')
   dev.off()
   
+<<<<<<< HEAD
+=======
+  
+  # Save a frame map for the sequence
+  if(FOI[1] != F) {
+    # Write out frame map:
+    flframe                  <- FOI[1]:FOI[2]
+    good                     <- array(0,length(frid))
+    good[flframe==goodfrrat] <- 1
+    fmap                     <- cbind(flframe,frid,frida,good)
+    colnames(fmap)           <- c("fluo_frame","fly_frame","arena_frame","good_frame")
+    write.csv(fmap,paste0(output_prefix,"_frame_map.csv"),row.names = F)
+  } else {
+    flframe                  <- 1:length(frid)
+    good                     <- array(0,length(frid))
+    good[flframe==goodfrrat] <- 1
+    fmap                     <- cbind(flframe,frid,frida,good)
+    colnames(fmap)           <- c("fluo_frame","fly_frame","arena_frame","good_frame")
+    write.csv(fmap,paste0(output_prefix,"_frame_map.csv"),row.names = F)
+  }
+  
+
+>>>>>>> parent of c5793d2... Merge pull request #28 from tjdawkins/master
   saveRDS(datrawint, paste0(output_prefix, "_datrawint.RDS"))
   write.table(datrawint, paste0(output_prefix, "_datrawint.csv"), sep = ",", row.names=F)
   
